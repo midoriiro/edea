465ebd8ccda6a6f07933c9c218e6359f62c0887c
bekhdadi alexis <alexis.bekhdadi@gmail.com>
2018-07-09 16:28:39 +0200

    style(test): change line feed from CRLF to LF

--- test/interval.test.ts
+++ test/interval.test.ts
@@ -4,63 +4,63 @@ import { Interval } from '../src/core/interval'
 describe('Note tests', () => {
   it('should pass when getting distance from two valid notes', () => {
     expect(Interval.getSemiToneFromNotes(NoteEnum.C, NoteEnum.E)).toEqual(4)
-  })
+  });
 
   it('should pass when getting distance from two valid notes and shifted notes list', () => {
     expect(Interval.getSemiToneFromNotes(NoteEnum.E, NoteEnum.A_SHARP)).toEqual(6)
-  })
+  });
 
   it('should pass when getting distance from same notes', () => {
     expect(Interval.getSemiToneFromNotes(NoteEnum.E, NoteEnum.E)).toEqual(12)
-  })
+  });
 
   it('should fail when getting distance and one note is not in range', () => {
     expect(() => {
       Interval.getSemiToneFromNotes(NoteEnum.C, 13)
     }).toThrow(Error)
-  })
+  });
 
   it('should pass when getting note from valid origin and semitone', () => {
     expect(Interval.getNoteFromSemitones(NoteEnum.C, 4)).toEqual(NoteEnum.E)
-  })
+  });
 
   it('should pass when getting note from shifted notes list', () => {
     expect(Interval.getNoteFromSemitones(NoteEnum.E, 6)).toEqual(NoteEnum.A_SHARP)
-  })
+  });
 
   it('should pass when getting note from octave', () => {
     expect(Interval.getNoteFromSemitones(NoteEnum.E, 12)).toEqual(NoteEnum.E)
-  })
+  });
 
   it('should fail when getting note with invalid origin', () => {
     expect(() => {
       Interval.getNoteFromSemitones(13, 1)
     }).toThrow(Error)
-  })
+  });
 
   it('should fail when getting note with invalid semitones', () => {
     expect(() => {
       Interval.getNoteFromSemitones(NoteEnum.E, 13)
     }).toThrow(Error)
-  })
+  });
 
   it('should pass when convert semitones to wholetones', () => {
     expect(Interval.toWholetones(5)).toEqual(2.5)
-  })
+  });
 
   it('should pass when convert wholetones to semitones', () => {
     expect(Interval.toSemitones(2.5)).toEqual(5)
-  })
+  });
 
   it('should pass when convert wholetones to semitones with invalid value', () => {
     expect(() => {
       Interval.toWholetones(0.5)
     }).toThrow(Error)
-  })
+  });
 
   it('should pass when convert semitones to wholetones with invalid value', () => {
     expect(() => {
       Interval.toSemitones(0.25)
     }).toThrow(Error)
   })
-})
+});
--- test/note.test.ts
+++ test/note.test.ts
@@ -4,56 +4,56 @@ import { Note } from '../src/core/note'
 
 describe('Note tests', () => {
   it('should pass with default parameters', () => {
-    let expectedArray: ExtendedArray = new ExtendedArray()
-    expectedArray.push(NoteEnum.C)
-    expectedArray.push(NoteEnum.D)
-    expectedArray.push(NoteEnum.E)
-    expectedArray.push(NoteEnum.F)
-    expectedArray.push(NoteEnum.G)
-    expectedArray.push(NoteEnum.A)
-    expectedArray.push(NoteEnum.B)
+    let expectedArray: ExtendedArray = new ExtendedArray();
+    expectedArray.push(NoteEnum.C);
+    expectedArray.push(NoteEnum.D);
+    expectedArray.push(NoteEnum.E);
+    expectedArray.push(NoteEnum.F);
+    expectedArray.push(NoteEnum.G);
+    expectedArray.push(NoteEnum.A);
+    expectedArray.push(NoteEnum.B);
 
     expect(Note.getNotes()).toEqual(expectedArray)
-  })
+  });
 
   it('should pass when include alteration notes is set to true', () => {
-    let expectedArray: ExtendedArray = new ExtendedArray()
-    expectedArray.push(NoteEnum.C)
-    expectedArray.push(NoteEnum.C_SHARP)
-    expectedArray.push(NoteEnum.D)
-    expectedArray.push(NoteEnum.D_SHARP)
-    expectedArray.push(NoteEnum.E)
-    expectedArray.push(NoteEnum.F)
-    expectedArray.push(NoteEnum.F_SHARP)
-    expectedArray.push(NoteEnum.G)
-    expectedArray.push(NoteEnum.G_SHARP)
-    expectedArray.push(NoteEnum.A)
-    expectedArray.push(NoteEnum.A_SHARP)
-    expectedArray.push(NoteEnum.B)
+    let expectedArray: ExtendedArray = new ExtendedArray();
+    expectedArray.push(NoteEnum.C);
+    expectedArray.push(NoteEnum.C_SHARP);
+    expectedArray.push(NoteEnum.D);
+    expectedArray.push(NoteEnum.D_SHARP);
+    expectedArray.push(NoteEnum.E);
+    expectedArray.push(NoteEnum.F);
+    expectedArray.push(NoteEnum.F_SHARP);
+    expectedArray.push(NoteEnum.G);
+    expectedArray.push(NoteEnum.G_SHARP);
+    expectedArray.push(NoteEnum.A);
+    expectedArray.push(NoteEnum.A_SHARP);
+    expectedArray.push(NoteEnum.B);
 
     expect(
       Note.getNotes({
         includeAlteration: true
       })
     ).toEqual(expectedArray)
-  })
+  });
 
   it('should pass when shifted note is settled in scope', () => {
-    let expectedArray: ExtendedArray = new ExtendedArray()
-    expectedArray.push(NoteEnum.F)
-    expectedArray.push(NoteEnum.G)
-    expectedArray.push(NoteEnum.A)
-    expectedArray.push(NoteEnum.B)
-    expectedArray.push(NoteEnum.C)
-    expectedArray.push(NoteEnum.D)
-    expectedArray.push(NoteEnum.E)
+    let expectedArray: ExtendedArray = new ExtendedArray();
+    expectedArray.push(NoteEnum.F);
+    expectedArray.push(NoteEnum.G);
+    expectedArray.push(NoteEnum.A);
+    expectedArray.push(NoteEnum.B);
+    expectedArray.push(NoteEnum.C);
+    expectedArray.push(NoteEnum.D);
+    expectedArray.push(NoteEnum.E);
 
     expect(
       Note.getNotes({
         shift: NoteEnum.F
       })
     ).toEqual(expectedArray)
-  })
+  });
 
   it('should fail when shifted note is out of bound', () => {
     expect(() => {
@@ -61,22 +61,22 @@ describe('Note tests', () => {
         shift: 13
       })
     }).toThrow(Error)
-  })
+  });
 
   it('should pass when include alteration notes is set to true and shifted note is settled in scope', () => {
-    let expectedArray: ExtendedArray = new ExtendedArray()
-    expectedArray.push(NoteEnum.F)
-    expectedArray.push(NoteEnum.F_SHARP)
-    expectedArray.push(NoteEnum.G)
-    expectedArray.push(NoteEnum.G_SHARP)
-    expectedArray.push(NoteEnum.A)
-    expectedArray.push(NoteEnum.A_SHARP)
-    expectedArray.push(NoteEnum.B)
-    expectedArray.push(NoteEnum.C)
-    expectedArray.push(NoteEnum.C_SHARP)
-    expectedArray.push(NoteEnum.D)
-    expectedArray.push(NoteEnum.D_SHARP)
-    expectedArray.push(NoteEnum.E)
+    let expectedArray: ExtendedArray = new ExtendedArray();
+    expectedArray.push(NoteEnum.F);
+    expectedArray.push(NoteEnum.F_SHARP);
+    expectedArray.push(NoteEnum.G);
+    expectedArray.push(NoteEnum.G_SHARP);
+    expectedArray.push(NoteEnum.A);
+    expectedArray.push(NoteEnum.A_SHARP);
+    expectedArray.push(NoteEnum.B);
+    expectedArray.push(NoteEnum.C);
+    expectedArray.push(NoteEnum.C_SHARP);
+    expectedArray.push(NoteEnum.D);
+    expectedArray.push(NoteEnum.D_SHARP);
+    expectedArray.push(NoteEnum.E);
 
     expect(
       Note.getNotes({
@@ -85,4 +85,4 @@ describe('Note tests', () => {
       })
     ).toEqual(expectedArray)
   })
-})
+});
